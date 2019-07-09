c519805fb1bb38639e8ea54a92f6e58f2cfd4f3c
syhan11 <sueyoung.6311@gmail.com>
2019-07-09 17:00:52 -0400

    init commit

---
+++ src/main/java/com/example/hobby/HomeCtrol.java
@@ -0,0 +1,27 @@
+package com.example.hobby;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeCtrol {
+    @RequestMapping("/home")
+    public String homePage(){
+        return ("home");
+    }
+
+    @RequestMapping("/photo")
+    public String infoPage(){
+        return ("photo");
+    }
+
+    @RequestMapping("/calendar")
+    public String calendarPage(){
+        return ("calendar");
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return ("about");
+    }
+}
